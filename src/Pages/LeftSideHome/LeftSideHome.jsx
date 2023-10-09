026d071e180e402100e95c84d4d64f4e1a0d09c2
emurmudu <eliasmurmudu@gmail.com>
2023-10-09 12:55:10 +0600

    two extra section added on home page

--- src/Pages/LeftSideHome/LeftSideHome.jsx
+++ src/Pages/LeftSideHome/LeftSideHome.jsx
@@ -1,8 +1,38 @@
 
+// IoCheckmarkSharp
+// IoCheckmarkDoneOutline
+// import { FaBee, } from 'react-icons/fa';
+import { IoMdCheckmark } from 'react-icons/io';
+import { BiSolidRightArrow } from 'react-icons/bi';
 const LeftSideHome = () => {
     return (
-        <div className=" pl-9 border">
-            <h1>Left side home</h1>
+        <div className=" pl-9 border p-4">
+            <div className='mt-6'>
+                <h1 className=' text-lg md:text-2xl mb-4 text-pink-400 font-bold'>Why Need Yoga </h1>
+                <ol className=' space-y-2'>
+                    <li className='flex items-center gap-1'> <BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow> <span>It Increases Flexibility</span> </li>
+                    <li className='flex items-center gap-1'><BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow><span>Enhances Balance</span></li>
+                    <li className='flex items-center gap-1'><BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow><span>It Reduces Your Stress</span></li>
+                    <li className='flex items-center gap-1'><BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow><span>It Connects Your Body And Mind</span></li>
+                    <li className='flex items-center gap-1'><BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow><span>It Boosts Your Energy Level</span></li>
+                    <li className='flex items-center gap-1'><BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow><span>It Improves Blood Circulation</span></li>
+                    <li className='flex items-center gap-1'><BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow><span>It Enhances Good Mood</span> </li>
+                    <li className='flex items-center gap-1'><BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow><span>Promotes Better Sleep</span></li>
+                    <li className='flex items-center gap-1'><BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow><span>It Controls Your Weight</span></li>
+                    <li className='flex items-center gap-1'><BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow><span>Increases Social Connection</span></li>
+                    <li className='flex items-center gap-1'><BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow><span>Improves Immune System</span></li>
+                    <li className='flex items-center gap-1'><BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow><span> It Triggers Spiritual Growth</span></li>
+                </ol>
+            </div>
+            <div>
+                <h1 className=' text-lg md:text-2xl mt-4 mb-4 text-pink-400 font-bold'>Membership </h1>
+                <ol className=' space-y-2'>
+                    <li className='flex items-center gap-1'> <BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow> <span>It Founding members</span> </li>
+                    <li className='flex items-center gap-1'> <BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow> <span>It Life Membership (10 years)</span> </li>
+                    <li className='flex items-center gap-1'> <BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow> <span>Personal membership</span> </li>
+                    <li className='flex items-center gap-1'> <BiSolidRightArrow className=' text-pink-400'></BiSolidRightArrow> <span>Institutional membership</span> </li>
+                </ol>
+            </div>
         </div>
     );
 };
--- src/Pages/RightSideHome/RightSideHome.jsx
+++ src/Pages/RightSideHome/RightSideHome.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import { useContext } from 'react';
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { AuthContext } from '../../Providers/AuthProvider';
+import { useNavigate } from 'react-router-dom';
+
 
 const RightSideHome = () => {
+    const { signInWithGoogle } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleGoogleSignIn = () => {
+        signInWithGoogle()
+            .then(result => {
+                console.log(result.user);
+                navigate("/");
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
     return (
         <div className=' pr-10 border'>
-            <h1>Right side home</h1>
+            <div className='p-4 space-y-3 mb-6'>
+                <h2 className=" text-lg md:text-2xl text-pink-400 font-bold">Login With</h2>
+                <button onClick={handleGoogleSignIn} className="btn btn-outline w-full">
+                    <FaGoogle></FaGoogle>
+                    Google
+                </button>
+
+            </div>
+            <div className='p-4 mb-6'>
+                <h2 className="text-lg md:text-2xl text-pink-400 font-bold mb-4">Find Us on</h2>
+                <a className='p-4 flex text-lg items-center border rounded-t-lg' href="https://www.facebook.com/" target='_blank' rel="noreferrer">
+                    <FaFacebook className='mr-3'></FaFacebook>
+                    <span>Facebook</span>
+                </a>
+                <a className='p-4 flex text-lg items-center border-x' href="https://twitter.com/" target='_blank' rel="noreferrer">
+                    <FaTwitter className='mr-2'></FaTwitter>
+                    <span>Twitter</span>
+                </a>
+                <a className='p-4 flex text-lg items-center border rounded-b-lg' href="https://www.instagram.com/" target='_blank' rel="noreferrer">
+                    <FaInstagram className='mr-2'></FaInstagram>
+                    <span>Instagram</span>
+                </a>
+            </div>
         </div>
     );
 };
